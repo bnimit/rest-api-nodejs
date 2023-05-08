37654d7a599f44dd414e8d726e413cde13965562
Nimit Bhandari <nimitbhandari@outlook.com>
2023-05-08 17:30:52 +0530

    Remove sample authentication

--- source/routes/posts.ts
+++ source/routes/posts.ts
@@ -1,6 +1,5 @@
 import express from "express";
 import controller from "../controllers/posts";
-import { checkJwt } from "../middleware/auth.middleware";
 
 const router = express.Router();
 
@@ -9,6 +8,5 @@ router.get("/api/v1/posts/:id", controller.getPost);
 router.put("/api/v1/posts/:id", controller.updatePost);
 router.delete("/api/v1/posts/:id", controller.deletePost);
 router.post("api/v1/posts", controller.addPost);
-router.use(checkJwt);
 
 export default router;
