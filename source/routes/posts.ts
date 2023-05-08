f9623c30294505f2967612f1c46eb8790ad303a3
Nimit Bhandari <nimitbhandari@outlook.com>
2023-05-08 13:29:25 +0530

    Add authorization middleware using Auth0

--- source/routes/posts.ts
+++ source/routes/posts.ts
@@ -1,12 +1,14 @@
 import express from "express";
 import controller from "../controllers/posts";
+import { checkJwt } from "../middleware/auth.middleware";
 
 const router = express.Router();
 
-router.get("/posts", controller.getAllPosts);
-router.get("/posts/:id", controller.getPost);
-router.put("/posts/:id", controller.updatePost);
-router.delete("/posts/:id", controller.deletePost);
-router.post("/posts", controller.addPost);
+router.get("/api/v1/posts", controller.getAllPosts);
+router.get("/api/v1/posts/:id", controller.getPost);
+router.put("/api/v1/posts/:id", controller.updatePost);
+router.delete("/api/v1/posts/:id", controller.deletePost);
+router.post("api/v1/posts", controller.addPost);
+router.use(checkJwt);
 
 export default router;
